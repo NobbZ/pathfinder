e1f51a116a1b0de0c16126fa74b7b5fda6df861e
Norbert Melzer <timmelzer@gmail.com>
2014-05-09 13:29:27 +0200

    read the board from file

--- Main.hs
+++ Main.hs
@@ -6,15 +6,55 @@ import           System.Environment
 import           System.Exit
 import           System.IO
 
+import           Data.Board
+import           Data.WayTree
+
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  boards <- getArgs
-  boards <- filterM doesFileExist boards -- getArgs
-  hPrint stderr boards
-  mapM solve boards
+  boardNames <- getArgs
+  existingBoardNames <- filterM doesFileExist boardNames
+  hPrint stderr existingBoardNames
+  mapM solve existingBoardNames
   return ()
 
 solve :: FilePath -> IO ()
 solve fp = do
   putStrLn fp
+  theBoard <- parseFile fp
+  theTree <- liftM growTree $ return theBoard
+  theWay <- liftM cut $ return theTree
+  thePath <- liftM toList $ return theWay
+  mapM print thePath
+  return ()
+
+parseFile :: FilePath -> IO Board
+parseFile fp = do
+  boardString <- readFile fp
+  boardLines <- liftM lines $ return boardString
+  hPrint stderr boardLines
+  boardBoard <- liftM parseBoard $ return boardLines
+  hPrint stderr boardBoard
+  return boardBoard
+
+parseBoard :: [String] -> Board
+parseBoard [[]]   = []
+parseBoard (l:[]) = [parseLine l]
+parseBoard (l:ls) = [parseLine l] ++ parseBoard ls
+
+parseLine l = map parseTile l
+parseTile t = case t of
+  'S' -> Start
+  'E' -> Exit
+  'X' -> Wall
+  ' ' -> Way
+
+growTree :: Board -> WayTree
+growTree = const (Walkable (0,0) Start NotWalkable NotWalkable NotWalkable NotWalkable)
+
+cut :: WayTree -> WayTree
+cut = id
+
+toList :: WayTree -> [Coords]
+toList = const [(0,0)]
+
